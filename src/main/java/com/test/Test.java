96e70670025005d8f6653c279148521345f61feb
george <676002187@.com>
2020-05-31 10:43:27 +0800

    第一次提交

---
+++ src/main/java/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * @author George Jia
+ * @email 676002187@qq.com
+ * @date 2020-05-30 16:37:33
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("psvm---");
+    }
+}
---
+++ src/test/java/com/maven/FirstMavenTest.java
@@ -0,0 +1,15 @@
+package com.maven;
+
+import org.junit.Test;
+
+/**
+ * @author George Jia
+ * @email 676002187@qq.com
+ * @date 2020-05-30 21:55:51
+ */
+public class FirstMavenTest {
+    @Test
+    public void testFirstMaven(){
+        System.out.println("the first maven demo . ");
+    }
+}
